efd617dc71f088bd78f1c50339182234e34e96cc
segun adebayo <eniolaadebayo24@gmail.com>
2025-02-20 12:29:01 +0100

    initial commit for reduxtkt cart

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home'
+import Cart from './pages/cart'
+import Header from './components/header'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route  path='/cart' element={<Cart />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
